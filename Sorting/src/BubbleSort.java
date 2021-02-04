f2a97aa6d178a66ac3caeaaea6b041a61c718747
Rahul.Verma <Rahul.Verma@RahulV-L1.bennett.edu.in>
2021-02-04 09:56:46 +0530

    bubble

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -3,7 +3,7 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is Bubble Sort Algorithm");
+		System.out.println("This is Bubble Sort");
 	}
 
 }
